48e89ed883e4d67b318192e9cec4258f77aaf19d
Shaib Echesa Makokha <echesaswahib@gmail.com>
2024-07-30 02:12:40 +0300

    Fix npm and node issues, add tests for Header and Article components

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,9 @@ import React from "react";
 
 function Header(props) {
       return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
+        <header>
+          <h1>hello from the Header!</h1>
+        </header>
       )
 }
 
